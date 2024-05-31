c23e1c7e6c3b943e45f3544557357e21cf0c04c3
민병욱 <vvxxxxk@gmail.com>
2024-05-31 17:18:30 +0900

    05/31

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -29,6 +29,7 @@ public class MappingController {
 
     /**
      * 편리한 축약 어노테이션
+     *
      * @GetMapping
      * @PostMapping
      * @PutMapping
@@ -44,6 +45,7 @@ public class MappingController {
     /**
      * PathVariable 사용
      * 변수명이 같으면 생략 가능
+     *
      * @PathVariable("userId") String userId -> @PathVariable String userId
      */
     @GetMapping("/mapping/{userId}")
@@ -58,6 +60,60 @@ public class MappingController {
     @GetMapping("/mapping/users/{userId}/orders/{orderId}")
     public String mappingPath(@PathVariable("userId") String userId, @PathVariable("orderId") Long orderId) {
         log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode"
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug"
+     * params={"mode=debug", "data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! = )
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 MediaType
+     * produces="text/html",
+     * produces="!text/html"
+     * produces="text/*"
+     * produces="*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
