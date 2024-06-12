bae9af90739945c8c7b19fcf3387375fc2bc6cc1
vvxxxxk <vvxxxxk@gmail.com>
2024-06-13 04:38:19 +0900

    06/13

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -121,6 +121,19 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUser(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUser(model);
+        return "basic/javascript";
+    }
+
     @Data
     static class User {
         private String username;
